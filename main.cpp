5743477ea051baed7299f0b44a7e0e50a4ac1263
john graham <john.graham@outlook.co.nz>
2018-01-01 03:50:11 +1300

    very minor code tidy-up.

--- main.cpp
+++ main.cpp
@@ -4,27 +4,28 @@ using namespace std;
 
 int main() {
 
-    // delcare a variable for holding an adress,
+    // Declare a variable for holding an address,
     // which are of a standard length.
-    // equalling nullptr is good practice.
+    // Sometimes initializing a pointer to 'nullptr' is good to do.
     int* int_ptr = nullptr;
 
-    //Setting the value of i to 39
+    //Setting the value of i to 39.
     int i = 39;
 
-    //setting up a 2nd reference to the integer  i;
-    int& int_ref = i;
+    //Setting up a 2nd reference to the integer  i.
+    int &int_ref = i;
 
-    // set the int_ptr to have the address of the variable i;
+    // [*] Set the int_ptr to have the address of the variable i.
     int_ptr = &i;
 
-    //printing to the console the de-referenced value at the address held by int_ptr;
+    //Pinting to the console the de-referenced value at the address held by int_ptr.
     cout << *int_ptr << endl;
 
-    //changing the value of int_ptr to now have the address of int_ref which happens to be the same
-    //address as i;
+    //[**]  Changing the value of int_ptr to now have the address of int_ref which happens to be the same
+    // Address as i.
+    // Compare [*] and [**].
     int_ptr = &int_ref;
 
-    //consequently the value obtained by de-referencing the contents of that same address is the same value, 39.
+    // Consequently the value obtained by de-referencing the contents of that same address is the same value, 39.
     cout << *int_ptr << endl;
 }
